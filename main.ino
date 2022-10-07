621dabb91720e8f52cda01a108ab0c97bb72fe36
lsheets <lsheets@astromotive.space>
2022-10-06 21:44:33 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -9,6 +9,10 @@
 
 int state = 0;
 
+char message[255];
+
+bool delimiterFlag = false;
+
 struct Motor {
   int dirPin;
   int stepPin;
@@ -18,27 +22,80 @@ struct Motor {
   int stepCounter;
   unsigned long stepDelay;    // micros
   int stepDelayAddress;       // EEPROM address where stepDelay lives
-}
+};
 
 
-Motor motor1 { dir1Pin, step1Pin, LOW, LOW, 0, 0, 0, 0 }
-Motor motor2 { dir2Pin, step2Pin, LOW, LOW, 0, 0, 0, 2 }
+unsigned int tubeLength;
+unsigned int tubeOffset;
+
+int tubeLengthAddress = 4;
+int tubeOffsetAddress = 6;
+
+
+Motor motorOne { dir1Pin, step1Pin, LOW, LOW, 0, 0, 0, 0 };
+Motor motorTwo { dir2Pin, step2Pin, LOW, LOW, 0, 0, 0, 2 };
 
 
 void setup() {
   Serial.begin(115200);
 
   // get stepDelay from addresses
-  motor1.stepDelay = readIntFromEEPROM(motor1.stepDelayAddress);
-  motor2.stepDelay = readIntFromEEPROM(motor2.stepDelayAddress);
+  motorOne.stepDelay = readIntFromEEPROM(motorOne.stepDelayAddress);
+  motorTwo.stepDelay = readIntFromEEPROM(motorTwo.stepDelayAddress);
 
+  tubeLength = readIntFromEEPROM(tubeLengthAddress);
+  tubeOffset = readIntFromEEPROM(tubeOffsetAddress);
+
+  // run motor homing function until it returns true i.e. until 
+  // it hits the proximity sensor, so we start with a known position
+  while (!motorHoming(motorOne)); 
   
 }
 
 void loop() {
-  if (Serial.available()) {
-    state = (int)Serial.read();
-  }
+
+  if (state == 0) {
+  
+    while (Serial.available()) {
+      char x = Serial.read();
+      if (x != "x") {
+        strcat(message, x); 
+      } else {
+        delimiterFlag = true;
+      }
+    }
+  
+    if (delimiterFlag) {
+      delimiterFlag = false;
+
+      int tempState = message[0];
+      unsigned int tempTubeLength = message[1]<<8 + message[2];
+      unsigned int tempTubeOffset = message[3]<<8 + message[4];
+      unsigned long tempMotorOneStepDelay = message[5]<<24 + message[6]<<16 + message[7]<<8 + message[8];
+      unsigned long tempMotorTwoStepDelay = message[9]<<24 + message[10]<<16 + message[11]<<8 + message[12];
+
+      if (state != tempState) {
+        state = tempState;
+      }
+      if (tubeLength != tempTubeLength) {
+        tubeLength = tempTubeLength;
+      }
+      if (tubeOffset != tempTubeOffset) {
+        tubeOffset = tempTubeOffset;
+      }
+      if (motorOne.stepDelay != tempMotorOneStepDelay) {
+        motorOne.stepDelay = tempMotorOneStepDelay;
+      }
+      if (motorTwo.stepDelay != tempMotorTwoStepDelay) {
+        motorTwo.stepDelay = tempMotorTwoStepDelay;
+      }
+          
+    }
+
+  } else if (state == 1) {
+    
+  } 
+  
   
 
 
@@ -46,24 +103,25 @@ void loop() {
 }
 
 
-bool motorHoming(motor) {
+bool motorHoming(Motor m) {
   bool homed = false;
-  int proxState = fastDigitalRead(PINB, prox1pin);
+  int proxState = fastDigitalRead(PINB, prox1Pin);
   if (proxState) {
     homed = true;
   } else {
-    motorStep(motor, 0);
+    motorStep(m, 0);
   }
   return homed;
 }
 
 
-void motorStep(motor, dir) {
-  if (micros() - motor.prevStep > motor.stepDelay) {
-    motor.prevStep = micros();
-    motor.stepState = motorStepState ? HIGH : LOW;  // toggle from high to low or vice versa
-    fastDigitalWrite(motor.dirPin, motor.dirState);
-    fastDigitalWrite(motor.stepPin, motor.stepState);
+void motorStep(Motor m, int dir) {
+  if (micros() - m.prevStep > m.stepDelay) {
+    m.prevStep = micros();
+    m.stepState = m.stepState ? HIGH : LOW;  // toggle from high to low or vice versa
+    m.stepCounter += (1 * m.stepState);
+    fastDigitalWrite(m.dirPin, m.dirState);
+    fastDigitalWrite(m.stepPin, m.stepState);
   }
 }
 
@@ -82,6 +140,9 @@ void fastDigitalWrite(int pin, int state) {
   }
 }
 
+
+
+
 void writeIntIntoEEPROM(int address, int number) { 
   EEPROM.write(address, number >> 8);
   EEPROM.write(address + 1, number & 0xFF);
@@ -89,6 +150,8 @@ void writeIntIntoEEPROM(int address, int number) {
 int readIntFromEEPROM(int address) {
   return (EEPROM.read(address) << 8) + EEPROM.read(address + 1);
 }
+
+
 
 
 void storeMotorSpeed(Motor m) {
