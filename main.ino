8d2adf15bf7af580461ba142326a40e734066d4f
lsheets <lsheets@astromotive.space>
2022-10-06 22:36:02 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,13 +1,12 @@
 #include <EEPROM.h>
 
-#define dir1Pin   0b00000001 // GPIO 8
-#define step1Pin  0b00000010 // GPIO 9
-#define dir2Pin   0b00000100 // GPIO 10
-#define step2Pin  0b00001000 // GPIO 11
-#define prox1Pin  0b00010000 // GPIO 12
-#define prox2Pin  0b00100000 // GPIO 13
+#define DIRPIN1   0b00000001 // GPIO 8
+#define STEPPIN1  0b00000010 // GPIO 9
+#define DIRPIN2   0b00000100 // GPIO 10
+#define STEPPIN2  0b00001000 // GPIO 11
+#define PROXPIN1  0b00010000 // GPIO 12
+#define PROXPIN2  0b00100000 // GPIO 13
 
-int state = 0;
 
 char message[255];
 
@@ -19,33 +18,62 @@ struct Motor {
   int dirState;
   int stepState;
   unsigned long prevStep;     // micros
-  int stepCounter;
-  unsigned long stepDelay;    // micros
-  int stepDelayAddress;       // EEPROM address where stepDelay lives
+  unsigned int stepCounter;
+  unsigned int stepDelay;     // micros
+  int stepDelayAddr;       // EEPROM address where stepDelay lives
 };
 
 
-unsigned int tubeLength;
-unsigned int tubeOffset;
+unsigned int tubeLength;      // mm
+unsigned int tubeOffset;      // mm
+int tubeLengthAddr = 4;
+int tubeOffsetAddr = 6;
 
-int tubeLengthAddress = 4;
-int tubeOffsetAddress = 6;
+
+const int mmPerRev = 10;      // mm
+const int fullStepsPerRev = 200;
+unsigned int microsteps;
+int microstepsAddr = 8;
+int stepsPerRev;
+float distPerStep;
 
 
-Motor motorOne { dir1Pin, step1Pin, LOW, LOW, 0, 0, 0, 0 };
-Motor motorTwo { dir2Pin, step2Pin, LOW, LOW, 0, 0, 0, 2 };
+int mode;
+int modeAddr = 10;
+int state = 0;
+
+
+Motor motorOne { DIRPIN1, STEPPIN1, LOW, LOW, 0, 0, 0, 0 };
+Motor motorTwo { DIRPIN2, STEPPIN2, LOW, LOW, 0, 0, 0, 2 };
 
 
 void setup() {
   Serial.begin(115200);
 
-  // get stepDelay from addresses
-  motorOne.stepDelay = readIntFromEEPROM(motorOne.stepDelayAddress);
-  motorTwo.stepDelay = readIntFromEEPROM(motorTwo.stepDelayAddress);
+  // sets Pins 8,9,10,11 to Outputs, Pins 12,13 to Inputs
+  DDRB = DDRB | 0b00001111;
+  DDRB = DDRB & 0b11001111;
 
-  tubeLength = readIntFromEEPROM(tubeLengthAddress);
-  tubeOffset = readIntFromEEPROM(tubeOffsetAddress);
+  // get stepDelay
+  motorOne.stepDelay = readIntFromEEPROM(motorOne.stepDelayAddr);
+  motorTwo.stepDelay = readIntFromEEPROM(motorTwo.stepDelayAddr);
 
+  // get tube length and offset
+  tubeLength = readIntFromEEPROM(tubeLengthAddr);
+  tubeOffset = readIntFromEEPROM(tubeOffsetAddr);
+
+  // get mode
+  mode = readIntFromEEPROM(modeAddr);
+
+  // get microstepping 
+  microsteps = readIntFromEEPROM(microstepsAddr);
+
+  // calc steps per revolution
+  stepsPerRev = fullStepsPerRev * microsteps;  
+
+  // calc distance per step
+  distPerStep = mmPerRev / stepsPerRev;
+  
   // run motor homing function until it returns true i.e. until 
   // it hits the proximity sensor, so we start with a known position
   while (!motorHoming(motorOne)); 
@@ -71,28 +99,52 @@ void loop() {
       int tempState = message[0];
       unsigned int tempTubeLength = message[1]<<8 + message[2];
       unsigned int tempTubeOffset = message[3]<<8 + message[4];
-      unsigned long tempMotorOneStepDelay = message[5]<<24 + message[6]<<16 + message[7]<<8 + message[8];
-      unsigned long tempMotorTwoStepDelay = message[9]<<24 + message[10]<<16 + message[11]<<8 + message[12];
+      unsigned int tempMotorOneStepDelay = message[5]<<8 + message[6];
+      unsigned int tempMotorTwoStepDelay = message[7]<<8 + message[8];
+      int tempMode = message[9];
+      int tempMicrosteps = message[10];
 
       if (state != tempState) {
         state = tempState;
       }
       if (tubeLength != tempTubeLength) {
         tubeLength = tempTubeLength;
+        writeIntIntoEEPROM(tubeLengthAddr, tubeLength);
       }
       if (tubeOffset != tempTubeOffset) {
         tubeOffset = tempTubeOffset;
+        writeIntIntoEEPROM(tubeOffsetAddr, tubeOffset);
       }
       if (motorOne.stepDelay != tempMotorOneStepDelay) {
         motorOne.stepDelay = tempMotorOneStepDelay;
+        writeIntIntoEEPROM(motorOne.stepDelayAddr, motorOne.stepDelay);
       }
       if (motorTwo.stepDelay != tempMotorTwoStepDelay) {
         motorTwo.stepDelay = tempMotorTwoStepDelay;
+        writeIntIntoEEPROM(motorTwo.stepDelayAddr, motorTwo.stepDelay);
+      }
+      if (mode != tempMode) {
+        mode = tempMode;
+        writeIntIntoEEPROM(modeAddr, mode);
+      }
+      if (microsteps != tempMicrosteps) {
+        microsteps = tempMicrosteps;
+        writeIntIntoEEPROM(microstepsAddr, microsteps);
+        stepsPerRev = fullStepsPerRev * microsteps;  
+        distPerStep = mmPerRev / stepsPerRev;
       }
           
     }
 
   } else if (state == 1) {
+
+    // move the first motor to the start position of the tube
+
+
+    // sequence depends on the mode
+
+
+    // 
     
   } 
   
@@ -105,7 +157,7 @@ void loop() {
 
 bool motorHoming(Motor m) {
   bool homed = false;
-  int proxState = fastDigitalRead(PINB, prox1Pin);
+  int proxState = fastDigitalRead(PINB, PROXPIN1);
   if (proxState) {
     homed = true;
   } else {
@@ -153,7 +205,3 @@ int readIntFromEEPROM(int address) {
 
 
 
-
-void storeMotorSpeed(Motor m) {
-  writeIntIntoEEPROM(m.stepDelayAddress, m.stepDelay);
-}
