072ea6cbff65fc7716a40b447cb33b9da97d6cb7
lsheets <lsheets@astromotive.space>
2022-10-12 09:03:43 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -8,8 +8,8 @@
 #define PROXPIN2  0b00100000 // GPIO 13
 
 
-char message[255];
-
+unsigned int message[7];
+int index = 0;
 bool delimiterFlag = false;
 
 struct Motor {
@@ -19,38 +19,38 @@ struct Motor {
   int stepState;
   unsigned long prevStep;     // micros
   unsigned int stepCounter;
-  unsigned int stepDelay;     // micros
+  unsigned long stepDelay;     // micros
   float pos;                  // mm or angle (deg);
 };
 
 
 unsigned int tubeLength;      // mm
 unsigned int tubeOffset;      // mm
-int tubeLengthAddr = 4;
-int tubeOffsetAddr = 6;
-
 unsigned int motorOneSpeed;   // mm/s
 unsigned int motorTwoSpeed;   // deg/s
-int motorOneSpeedAddr = 0;
-int motorTwoSpeedAddr = 2;
-
 
 const int mmPerRev = 10;      // mm
 const int fullStepsPerRev = 200;
 unsigned int microsteps;
-int microstepsAddr = 8;
 int stepsPerRev;
 float distPerStep;
 float degPerStep;
 
-
-int mode;
-int modeAddr = 10;
+unsigned int mode;
 int state = 0;
+int proxDebounce = 0;
+
+int motorOneSpeedAddr = 10;
+int motorTwoSpeedAddr = 12;
+int tubeLengthAddr = 14;
+int tubeOffsetAddr = 16;
+int microstepsAddr = 18;
+int modeAddr = 20;
 
 
-Motor motorOne { DIRPIN1, STEPPIN1, LOW, LOW, 0, 0, 0, 0 };
-Motor motorTwo { DIRPIN2, STEPPIN2, LOW, LOW, 0, 0, 0, 0 };
+
+Motor motorOne { DIRPIN1, STEPPIN1, LOW, LOW, 0, 0, 0, 0.0 };
+Motor motorTwo { DIRPIN2, STEPPIN2, LOW, LOW, 0, 0, 0, 0.0 };
 
 
 void setup() {
@@ -63,6 +63,7 @@ void setup() {
   // get motor speeds
   motorOneSpeed = readIntFromEEPROM(motorOneSpeedAddr);
   motorTwoSpeed = readIntFromEEPROM(motorTwoSpeedAddr);
+
 
   // get tube length and offset
   tubeLength = readIntFromEEPROM(tubeLengthAddr);
@@ -85,40 +86,71 @@ void setup() {
   motorOne.stepDelay = (unsigned int)((1 / (motorOneSpeed / distPerStep) * 0.5) * 1e6);
   motorTwo.stepDelay = (unsigned int)((1 / (motorTwoSpeed / degPerStep) * 0.5) * 1e6);
 
+
+  // testing
+  motorOne.stepDelay = 100000;
+
+
+  Serial.print("Tube Length: ");
+  Serial.println(tubeLength);
+  Serial.print("Tube Offset: ");
+  Serial.println(tubeOffset);
+  Serial.print("Motor 1 Speed: ");
+  Serial.println(motorOneSpeed); 
+  Serial.print("Motor 2 Speed: ");
+  Serial.println(motorTwoSpeed); 
+  Serial.print("Mode: ");
+  Serial.println(mode); 
+  Serial.print("Microsteps: ");
+  Serial.println(microsteps); 
   
   // run motor homing function until it returns true i.e. until 
   // it hits the proximity sensor, so we start with a known position
-  while (!motorHoming(motorOne)); 
+  Motor *ptr = &motorOne;
+  while (!motorHoming( ptr )); 
+
+  Serial.println( "Homed!" );
+
   
 }
 
 void loop() {
 
   if (state == 0) {
-  
-    while (Serial.available()) {
-      char x = Serial.read();
-      if (x != "x") {
-        strcat(message, x); 
-      } else {
-        delimiterFlag = true;
+
+    if (Serial.available()) {
+      while (Serial.available()) {
+        char x = Serial.read();
+        unsigned int n = x - '0';
+        if (index < sizeof(message)) {
+          message[index] = n;
+          index++;
+        }
       }
+      for (int i = 0; i < sizeof(message); i++) {
+        Serial.print(message[i]);
+      }
+      Serial.println();
+      delimiterFlag = true;
+      index = 0;
     }
-  
+    
+    
     if (delimiterFlag) {
       delimiterFlag = false;
 
       int tempState = message[0];
-      unsigned int tempTubeLength = message[1]<<8 + message[2];
-      unsigned int tempTubeOffset = message[3]<<8 + message[4];
-      unsigned int tempMotorOneSpeed = message[5]<<8 + message[6];
-      unsigned int tempMotorTwoSpeed = message[7]<<8 + message[8];
-      int tempMode = message[9];
-      int tempMicrosteps = message[10];
+      unsigned int tempTubeLength = message[1];
+      unsigned int tempTubeOffset = message[2];
+      unsigned int tempMotorOneSpeed = message[3];
+      unsigned int tempMotorTwoSpeed = message[4];
+      int tempMode = message[5];
+      int tempMicrosteps = message[6];
+      
 
-      if (state != tempState) {
-        state = tempState;
-      }
+//      if (state != tempState) {
+//        state = tempState;
+//      }
       if (tubeLength != tempTubeLength) {
         tubeLength = tempTubeLength;
         writeIntIntoEEPROM(tubeLengthAddr, tubeLength);
@@ -157,7 +189,8 @@ void loop() {
     motorOne.dirState = HIGH;
     
     while (motorOne.pos < tubeOffset) {
-      motorStep(motorOne);
+      Motor *ptr = &motorOne;
+      motorStep( ptr );
     }
 
     delay(1000);
@@ -167,11 +200,17 @@ void loop() {
     // small amount. if mode 2, then the rotar motor will do a full
     // rotation, and then the linear motor will move a small amount.
 
-    // calc how many steps the linear motor has to move each time
+    if (mode) {    
 
-    // calc how many steps the rotary motor has to move each time
+      // calc how many steps the linear motor has to move each time
+      int  steps = distPerStep * tubeLength;
+      
+  
+      // calc how many steps the rotary motor has to move each time
+  
+      // calc how many total sequences to scan entire tube
 
-    // calc how many total sequences to scan entire tube
+    }
     
   } 
   
@@ -182,31 +221,39 @@ void loop() {
 }
 
 
-bool motorHoming(Motor m) {
+bool motorHoming(Motor *m) {
   bool homed = false;
   int proxState = fastDigitalRead(PINB, PROXPIN1);
+    
   if (proxState) {
-    homed = true;
-    m.pos = 0;      // set position to zero
+    proxDebounce += 1;
+
+    if (proxDebounce > 10) {
+      homed = true;
+      m->pos = 0;      // set position to zero
+    }
   } else {
-    motorStep(m);
+    motorStep( m );
+    proxDebounce = 0;
   }
   return homed;
 }
 
 
-void motorStep(Motor m) {
-  if (micros() - m.prevStep > m.stepDelay) {
-    m.prevStep = micros();
-    m.stepState = m.stepState ? HIGH : LOW;  // toggle from high to low or vice versa
+void motorStep(Motor *m) {
+  if (micros() - m->prevStep > m->stepDelay) {
+    m->prevStep = micros();
+    m->stepState = (m->stepState) ? LOW : HIGH;  // toggle from high to low or vice versa
 
+//    Serial.println(m->stepCounter);
+    
     // only increment when stepState is HIGH. If dir is 0, then it will multiply by 
     // -1, thus decrementing stepCounter, otherwise if dir is 1, then it will multiply 
     // by 1, incrementing stepCounter
-    m.stepCounter += (1 * m.stepState) * (2 * m.dirState - 1);
+    m->stepCounter += (1 * m->stepState) * (2 * m->dirState - 1);
     
-    fastDigitalWrite(m.dirPin, m.dirState);
-    fastDigitalWrite(m.stepPin, m.stepState);
+    fastDigitalWrite(m->dirPin, m->dirState);
+    fastDigitalWrite(m->stepPin, m->stepState);
   }
 }
 
@@ -225,14 +272,13 @@ void fastDigitalWrite(int pin, int state) {
   }
 }
 
-void writeIntIntoEEPROM(int address, int number) { 
+void writeIntIntoEEPROM(int address, unsigned int number) { 
   EEPROM.write(address, number >> 8);
   EEPROM.write(address + 1, number & 0xFF);
 }
 
-int readIntFromEEPROM(int address) {
+unsigned int readIntFromEEPROM(int address) {
   return (EEPROM.read(address) << 8) + EEPROM.read(address + 1);
 }
-
 
 
